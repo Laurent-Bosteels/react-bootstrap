7b966f599b136c8cf0ecf44e4b22ba27b0f4bc59
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-07-29 11:34:51 +0200

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import './App.css';
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {Home} from './Home'
-import {About} from './About'
-import {Contact} from './Contact'
-import {NoMatch} from './NoMatch'
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Contact } from "./Contact";
+import { NoMatch } from "./NoMatch";
+import { Layout } from "./components/Layout";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
 
       <React.Fragment>
-      <Router>
-      <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route component={NoMatch} />
-      </Switch>
-      </Router>
+        <Navigation />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
       </React.Fragment>
-      
+
     </div>
   );
 }
