986423d4aea1972e181b9deb4c1dc155b6ad3dce
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-07-29 11:23:46 +0200

    page routes

---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Contact = () => (
+        <div>
+            <h1>Contact</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sit quibusdam dolor nisi eius accusantium ipsum optio, aliquid enim quia excepturi deleniti consectetur tempora. Non sunt quae quidem alias ipsa.</p>
+        </div>
+    )
